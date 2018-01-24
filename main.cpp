993dd388df029acf8048d4a373320948c968b92c
Luay Alshawi sp3 <loay.vb@gmail.com>
2018-01-24 10:23:11 -0800

    register c++ backend chat server to be used in qml

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include "chatserver.h"
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ChatServer>("challenge.chatserver", 1, 0, "ChatServer");
+
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
